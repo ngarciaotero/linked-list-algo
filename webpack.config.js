7182807b6eb7a9d0e8945e73f488d0f3edd9b5ba
ngarciaotero <nasleygarciaotero@gmail.com>
2024-08-23 14:50:24 -0400

    refactor webpack configuration for development
    - remove unnecessary webpack plugins and loaders for a node.js cli app
    - consolidate webpack configuration into a single 'webpack.config.js'
    - uninstalled webpack-merge as it no longer needed
    - update package.json scripts to focus on development build and watch

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  target: "node",
+  mode: "development",
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
